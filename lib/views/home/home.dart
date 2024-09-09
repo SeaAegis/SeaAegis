18b1fab109430b7911f26c2ed5a8da1e80922e8b
AbhishekChukka <john.chukka@gmail.com>
2024-09-09 17:43:37 +0530

    home image to beach fetch

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -2,23 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
 import 'package:seaaegis/providers/app_theme.dart';
-import 'package:seaaegis/testApi/tester1.dart';
 import 'package:seaaegis/views/alerts/alert_message.dart';
 import 'package:seaaegis/views/beach_data/beach_stats.dart';
 import 'package:seaaegis/views/favorites/favorite.dart';
-import 'package:seaaegis/views/home/widgets/search_text_field.dart';
-
 import 'package:seaaegis/widgets/basic_app_bar.dart';
+import 'package:seaaegis/views/home/widgets/search_text_field.dart';
+import 'package:seaaegis/testApi/tester1.dart'; // For fetchBeachConditions
 
 class HomeScreen extends StatelessWidget {
   final Function()? onBack;
+
   HomeScreen({super.key, this.onBack});
+
   final List<Beach> beaches = [
-    Beach(image: 'assets/images/2633.jpg', name: 'Beach 1'),
-    Beach(image: 'assets/images/5352.jpg', name: 'Beach 2'),
-    Beach(image: 'assets/images/6728.jpg', name: 'Beach 3'),
-    Beach(image: 'assets/images/2151794936.jpg', name: 'Beach 4'),
+    Beach(
+      image: 'assets/images/2633.jpg',
+      name: 'Goa',
+      lat: 15.539930,
+      lon: 73.763687,
+    ),
+    Beach(
+      image: 'assets/images/5352.jpg',
+      name: 'Bapatla',
+      lat: 15.895520,
+      lon: 80.467890,
+    ),
+    Beach(
+      image: 'assets/images/6728.jpg',
+      name: 'Perupalem',
+      lat: 16.414021,
+      lon: 81.607384,
+    ),
+    Beach(
+      image: 'assets/images/2151794936.jpg',
+      name: 'Rushikonda Beach',
+      lat: 17.801910,
+      lon: 83.397888,
+    ),
   ];
+
+  Future<void> _navigateToBeachStats(BuildContext context, Beach beach) async {
+    // Fetch the beach conditions
+    List<BeachConditions> beachConditions =
+        await fetchBeachConditions(beach.lat, beach.lon);
+
+    if (beachConditions.isNotEmpty) {
+      // Pass the fetched conditions to BeachStats Page
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => BeachStats(
+            beachConditions:
+                beachConditions[0], // Show the first condition for now
+            conditionList: beachConditions, // Pass the entire list if needed
+          ),
+        ),
+      );
+    } else {
+      // Handle case when no conditions are fetched
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('No beach conditions available for ${beach.name}.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppThemeProvider>(builder: (context, provider, _) {
@@ -34,9 +81,7 @@ class HomeScreen extends StatelessWidget {
             IconButton(
                 onPressed: () {},
                 icon: const Icon(size: 32, Icons.notifications_outlined)),
-            const SizedBox(
-              width: 24,
-            )
+            const SizedBox(width: 24),
           ],
         ),
         body: Padding(
@@ -53,15 +98,11 @@ class HomeScreen extends StatelessWidget {
                       fontWeight: FontWeight.w600,
                       color: Colors.black),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
+                const SizedBox(height: 10),
                 const SearchTextField(
                   hintText: 'Search location...',
                 ),
-                const SizedBox(
-                  height: 14,
-                ),
+                const SizedBox(height: 14),
                 Container(
                   padding: const EdgeInsets.all(8),
                   decoration: BoxDecoration(
@@ -78,86 +119,74 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 18,
-                ),
+                const SizedBox(height: 18),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     const Text(
-                      'Populor Beaches',
+                      'Popular Beaches',
                       style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    TextButton(onPressed: () {}, child: const Text('See More'))
+                    TextButton(onPressed: () {}, child: const Text('See More')),
                   ],
                 ),
-                const SizedBox(
-                  height: 12,
-                ),
+                const SizedBox(height: 12),
                 GridView.builder(
                   shrinkWrap: true,
                   itemCount: beaches.length,
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      mainAxisSpacing: 8,
-                      crossAxisSpacing: 8,
-                      crossAxisCount: 2),
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    crossAxisCount: 2,
+                  ),
                   itemBuilder: (context, index) {
                     return GestureDetector(
-                      onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => BeachStats(
-                                beachConditions: BeachConditions(
-                                  airTempC: 23.0,
-                                  waterTempC: 24.0,
-                                  windSpeedMps: 5.0,
-                                  waveHeightM: 1.5,
-                                  swellHeightM: 1.2,
-                                  visibilityKm: 10.0,
-                                  humidity: 65.0,
-                                  precipitation: 0.0,
-                                  pressure: 1015.0,
-                                  time: DateTime.now(), // No const here
-                                  currentDirection: 120,
-                                ),
-                                conditionList: const [],
-                              ))),
+                      onTap: () =>
+                          _navigateToBeachStats(context, beaches[index]),
                       child: Container(
                         width: 140,
                         height: 160,
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage(beaches[index].image))),
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage(beaches[index].image),
+                          ),
+                        ),
                         child: Stack(
                           children: [
                             Positioned(
-                                right: 10,
-                                top: 10,
-                                child: CircleAvatar(
-                                    backgroundColor: Colors.grey.shade100,
-                                    child: const Icon(
-                                      Icons.favorite_border,
-                                      color: Colors.black,
-                                      size: 28,
-                                    ))),
+                              right: 10,
+                              top: 10,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.grey.shade100,
+                                child: const Icon(
+                                  Icons.favorite_border,
+                                  color: Colors.black,
+                                  size: 28,
+                                ),
+                              ),
+                            ),
                             Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  beaches[index].name,
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.black),
-                                ))
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                beaches[index].name,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ),
                     );
                   },
-                )
+                ),
               ],
             ),
           ),
@@ -181,13 +210,12 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
     _controller = PersistentTabController(initialIndex: 0);
   }
 
   List<Widget> _buildScreens() {
     return [
-      HomeScreen(),
+      HomeScreen(), // This is the updated HomeScreen
       const Favorites(),
       const AlertMessage(),
     ];
@@ -216,47 +244,23 @@ class _HomePageState extends State<HomePage> {
         contentPadding: 2,
         iconSize: 30,
         textStyle: const TextStyle(fontSize: 18),
-        // inactiveIcon: const Icon(Icons.calendar_month_outlined),
         icon: const Icon(Icons.warning),
         title: "Alerts",
         inactiveColorPrimary: Colors.grey,
       ),
-      // PersistentBottomNavBarItem(
-      //   contentPadding: 2,
-      //   iconSize: 30,
-      //   textStyle: const TextStyle(fontSize: 18),
-      //   inactiveIcon: const Icon(Icons.notifications),
-      //   icon: const Icon(Icons.notifications_outlined),
-      //   title: "Notifications",
-      //   inactiveColorPrimary: Colors.grey,
-      // ),
-      // PersistentBottomNavBarItem(
-      //   contentPadding: 2,
-      //   iconSize: 30,
-      //   textStyle: const TextStyle(fontSize: 18),
-      //   inactiveIcon: const Icon(Icons.person_2_outlined),
-      //   icon: const Icon(Icons.person),
-      //   title: "Profile",
-      //   inactiveColorPrimary: Colors.grey,
-      // ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       body: PersistentTabView(
         context,
         controller: _controller,
         screens: _buildScreens(),
         items: _navBarsItems(),
-
         resizeToAvoidBottomInset: true,
-
         navBarHeight: 60,
-        // padding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
-
         decoration: NavBarDecoration(
           borderRadius: BorderRadius.circular(10.0),
           colorBehindNavBar: Colors.white,
@@ -268,17 +272,6 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        // popAllScreensOnTapOfSelectedTab: true,
-        // popActionScreens: PopActionScreensType.all,
-        // itemAnimationProperties: const ItemAnimationProperties(
-        //   duration: Duration(milliseconds: 200),
-        //   curve: Curves.ease,
-        // ),
-        // screenTransitionAnimation: const ScreenTransitionAnimation(
-        //   animateTabTransition: true,
-        //   curve: Curves.ease,
-        //   duration: Duration(milliseconds: 200),
-        // ),
         navBarStyle: NavBarStyle.style1,
         isVisible: showNav,
       ),
@@ -289,6 +282,13 @@ class _HomePageState extends State<HomePage> {
 class Beach {
   final String image;
   final String name;
+  final double lat;
+  final double lon;
 
-  Beach({required this.image, required this.name});
+  Beach({
+    required this.lat,
+    required this.lon,
+    required this.image,
+    required this.name,
+  });
 }
