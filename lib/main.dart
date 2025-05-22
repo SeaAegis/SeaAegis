00e43ae7c600ca10c2c5c821a35628b2e339b7b3
LokeshSuryaPrakashK <lokeshsuryaprakashk@gmail.com>
2025-05-23 04:11:29 +0530

    Refactor project configuration and update dependencies; remove unused files and optimize code structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,7 @@ import 'package:seaaegis/firebase_options.dart';
 // import 'package:seaaegis/views/home/google_maps.dart';
 // import 'package:seaaegis/screens/home/searchbar.dart';
 import 'package:seaaegis/helpers/theme_data.dart';
-import 'package:seaaegis/screens/home/userselection.dart';
-import 'package:seaaegis/views/auth/user/login.dart';
-import 'package:seaaegis/views/home/home.dart';
-import 'routes/routes.dart';
+import 'package:seaaegis/views/auth/userselection.dart';
 
 // import 'package:seaaegis/views/home/widgets/searchbar.dart';
 @pragma('vm:entry-point')
